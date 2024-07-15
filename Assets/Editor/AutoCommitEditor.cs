62ef1f2f12eb48a24a553f64cdd74083ffe3a493
LEE SANG GYU <tkdrb062@gmail.com>
2024-07-16 01:39:26 +0900

    commit

--- Assets/Editor/AutoCommitEditor.cs
+++ Assets/Editor/AutoCommitEditor.cs
@@ -1,16 +1,25 @@
 using System.Runtime.InteropServices;
 using UnityEditor;
+using UnityEngine;
 
-public class AutoCommitEditor : Editor
+public class AutoCommitEditor : EditorWindow
 {
+    public static string commitMessage;
     [DllImport("msvcrt.dll", EntryPoint="system")]
     static extern int system(string command);
     
     [MenuItem("Tools/Auto Commit")]
     public static void AutoCommit()
     {
-        system("git add .");
-        system("git commit -m \"yasashi\"");
-        system("git push");
+        GetWindow<AutoCommitEditor>("Auto Commit");
+    }
+    
+    private void OnGUI()
+    {
+        commitMessage = EditorGUILayout.TextField("Commit Message", commitMessage);
+        if (GUILayout.Button("Commit"))
+        {
+            system($"git add . && git commit -m \"{commitMessage}\" && git push");
+        }
     }
 }
